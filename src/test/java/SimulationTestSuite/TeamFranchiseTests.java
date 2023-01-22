257188846e9ba17e768be323cb828d2e09b2bc4b
Aarnob Guha <aarnobguha@gmail.com>
2023-01-22 21:36:09 +1300

    began writing unit tests for team franchise super class

---
+++ src/test/java/SimulationTestSuite/TeamFranchiseTests.java
@@ -0,0 +1,62 @@
+package SimulationTestSuite;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import SimulationMain.Player;
+import org.junit.jupiter.api.Test;
+
+import SimulationMain.TeamFranchise;
+import SimulationMain.UserTeamFranchise;
+import SimulationMain.ComputerTeamFranchise;
+
+public class TeamFranchiseTests {
+    public static class TestSuperMethods {
+        @Test
+        public void testControlledByUser() {
+            TeamFranchise computerTeam = new ComputerTeamFranchise("computer team");
+            TeamFranchise userTeam = new UserTeamFranchise("user team");
+            assertTrue(userTeam.controlledByUser());
+            assertFalse(computerTeam.controlledByUser());
+        }
+
+        @Test
+        public void testConstructorInitialisation() {
+            TeamFranchise myTeam = new ComputerTeamFranchise("England");
+            assertEquals("England", myTeam.getName());
+            assertEquals(9000, myTeam.getPurse());
+            assertEquals(0, myTeam.getPoints());
+            assertEquals(0, myTeam.getNRR());
+        }
+
+        @Test
+        public void testAddingAffordablePlayer() {
+            int[] viratKohliStats = {207, 6283, 168, 4835, 4, 368, 251};
+            Player viratKohli = new Player("Virat Kohli", 200, false, false,
+                    viratKohliStats);
+            TeamFranchise myTeam = new UserTeamFranchise("India");
+
+            viratKohli.newBid(1500);
+            myTeam.addPlayer(viratKohli);
+            assertEquals(7500, myTeam.getPurse());
+        }
+
+        @Test
+        public void testAddingUnaffordablePlayer() {
+            int[] viratKohliStats = {207, 6283, 168, 4835, 4, 368, 251};
+            Player viratKohli = new Player("Virat Kohli", 200, false, false,
+                    viratKohliStats);
+            TeamFranchise myTeam = new UserTeamFranchise("India");
+
+            viratKohli.newBid(100000);
+            try {
+                myTeam.addPlayer(viratKohli);
+                fail();
+            } catch (ArithmeticException e) {
+
+            }
+        }
+    }
+}
